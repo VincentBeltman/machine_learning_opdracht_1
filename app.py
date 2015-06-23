e5e771ff3c636904c21c37f63167c19896ec3d0e
vincent <vbeltman@gmail.com>
2015-06-23 14:44:42 +0200

    trolol

--- app.py
+++ app.py
@@ -1,117 +1,89 @@
 
-from numpy import *
-from stringVals import sv
-import matplotlib.pyplot as plt
-from sklearn import linear_model, cross_validation
-from sklearn.metrics import *
-from sklearn.decomposition  import PCA as skPCA
 
-def readTrainingSet(name):
-	with open(name) as dataset:
-		lines = dataset.readlines()
-		filtered = []
-		numberOfLines = 0
-		for line in lines:
-			line = line.split(',')
-			if len(line) == 26:
-				for i, item in enumerate(line):
-					if item == "?":
-						break
+def readGenres():
+	genres = []
+	with open('data/u.genre') as f:
+		for line in f.readlines():
+			genre = line.split('|')[0]
+			if '\n' not in genre:
+				genres.append(genre)
+	return genres
+
+def readUsers():
+	users = {}
+	with open('data/u.user') as f:
+		for line in f.readlines():
+			data = line.split('|')
+			users[data[0]] = {
+				'age': int(data[1]),
+				'gender': data[2],
+				'occupation': data[3],
+				'zipcode': data[4].strip('\n')
+			}
+	return users
+
+def readMovies(genres):
+	movies = {}
+	with open('data/u.item') as f:
+		for line in f.readlines():
+			data = line.split('|')
+			movies[data[0]] = {
+				'title': data[1],
+				# 'release_date': data[2],
+				# 'video_release_date': data[3],
+				# 'imdb_url': data[4]
+			}
+			movies[data[0]]['genres'] = []
+			for index, isGenre in enumerate(data[5:]):
+				if isGenre.strip('\n') == '1':
+					movies[data[0]]['genres'].append(index)
+	return movies
+
+def printStatistics(genres, users, movies):
+	genderStatistic = {}
+	with open('data/u.data') as f:
+		for index, line in enumerate(f.readlines()):
+			data = line.split('\t')
+			user = users[data[0]]
+			movie = movies[data[1]]
+			rating = int(data[2])
+
+			for genre in movie['genres']:
+				genreKey = user['gender'] + str(genre)
+				if genreKey in genderStatistic:
+					genderStatistic[genreKey] += 1
 				else:
-					filtered.append(line)
-					numberOfLines += 1
+					genderStatistic[genreKey] = 1
+	print getTop5(genderStatistic)
 
-		stringFeatures = [2, 3, 4, 5, 6, 7, 8, 14, 15, 17]
-		floatFeatures = [0, 1, 9, 10, 11, 12, 13, 16, 18, 19, 20, 21, 22, 23, 24]
-		N = len(stringFeatures) + len(floatFeatures)
-
-		returnMat = zeros((numberOfLines, N), dtype=float)
-		answers = []
-
-		for i, line in enumerate(filtered):
-			listFromLine = []
-			add = True
-			for j, item in enumerate(line):
-				if j in stringFeatures:
-					listFromLine.append(float(sv[j].index(item)))
-				elif j in floatFeatures:
-					listFromLine.append(float(item))
-				elif j == 25:
-					item = float(item)
-					answers.append(item)
-			else:
-				returnMat[i,:] = listFromLine
-
-		return returnMat, answers
-
-def scatter(xas, yas, i):
-	plt.legend(loc=2)
-	plt.xlabel(i)
-	plt.ylabel("Car price")
-	plt.scatter(xas, yas, c='g')
-
-def plot(xas, line):
-	plt.plot(xas, line, color='red', linewidth=3)
-	plt.show()
-
-def execLinearRegression(trainingset , trainingLabels, folds=8):
-	print("LinearRegression numberOfFolds " , folds)
-	clf = linear_model.LinearRegression()
-	kf = cross_validation.KFold(len(trainingset), n_folds=folds, shuffle=True)
-	scores = []
-
-	for train_index, test_index in kf:
-		clf.fit(trainingset[train_index], array(trainingLabels)[train_index])
-		score = clf.score(trainingset[test_index], array(trainingLabels)[test_index])
-		print "Score:", score
-		scores.append(score)
-		# i = 0
-		# scatter(trainingset[train_index][:,i], array(trainingLabels)[train_index], i)
-		# plot(trainingset[test_index][:,i], clf.predict(trainingset[test_index]))
-	print("\n")
-	return calcAvgScore(scores, folds)
-
-def calcAvgScore(scores, folds):
-	highest = -1
-	lowest  = 1
-	total   = 0
-	for score in scores:
-		if score > highest:
-			highest = score
-		elif score < lowest:
-			lowest = score
-		total += score
-	if len(scores) >= 6:
-		total -= highest
-		total -= lowest
-		return total/(folds-2)
-	return total/folds
-
-def execPCA(trainingset , trainingLabels):
-	print("PCA")
-	for i in range(1 , 10):
-		pca = skPCA(n_components = i)
-		transformedData = pca.fit(trainingset).transform(trainingset)
-		
-		clf = linear_model.LinearRegression()
-		clf.fit(transformedData , trainingLabels)
-		print "Number of Components: " , i ,"score " , clf.score(transformedData , trainingLabels)#, "Variantie" , pca.explained_variance_ratio_;
-	for i in range(1 , 10):
-		pca = skPCA(n_components = i , whiten = True)
-		transformedData = pca.fit(trainingset).transform(trainingset)
-		
-		clf = linear_model.LinearRegression()
-		clf.fit(transformedData , trainingLabels)
-		print "Whiten True Number of Components: " , i ,"score " , clf.score(transformedData , trainingLabels)#, "Variantie" , pca.explained_variance_ratio_;
-
+def getTop5(statistic):
+	highestMen = [0, 0, 0, 0, 0]
+	highestFemale = [0, 0, 0, 0, 0]
+	resultMen = []
+	resultFemale = []
+	for key, val in statistic.items():
+		if key.startswith('M'):
+			highest = highestMen
+			result = resultMen
+		else:
+			highest = highestFemale
+			result = resultFemale
+		if val > highest[-1]:
+			for index, high in enumerate(highest):
+				if val > high:
+					highest = highest[:index] + [val] + highest[index:-1]
+					result = result[:index] + [{key: val}] + result[index:-1]
+					break
+		if key.startswith('M'):
+			highestMen = highest
+			resultMen = result
+		else:
+			highestFemale = highest
+			resultFemale = result
+	return highestFemale
 
 if __name__ == "__main__":
-	trainingset, trainingLabels = readTrainingSet("autoprice.txt")
-	scores = {}
-	for i in range(2, 9):
-		scores[i] = execLinearRegression(trainingset , trainingLabels, i)
-	print "Score tabel:"
-	print "Nr folds\t| Score"
-	for fold, score in scores.items():
-		print fold, '\t\t|', score
-	#execPCA(trainingset , trainingLabels)
+	genres = readGenres()
+	users = readUsers()
+	movies = readMovies(genres)
+	data = printStatistics(genres, users, movies)
